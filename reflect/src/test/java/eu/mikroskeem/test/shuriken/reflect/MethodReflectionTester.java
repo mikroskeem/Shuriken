31f02a8d7f81f959a4789471c97c6bd8b9ee2f52
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-03-21 16:26:29 +0200

    Add primitive/object array test

--- reflect/src/test/java/eu/mikroskeem/test/shuriken/reflect/MethodReflectionTester.java
+++ reflect/src/test/java/eu/mikroskeem/test/shuriken/reflect/MethodReflectionTester.java
@@ -2,6 +2,7 @@ package eu.mikroskeem.test.shuriken.reflect;
 
 import eu.mikroskeem.shuriken.reflect.Reflect;
 import eu.mikroskeem.shuriken.reflect.wrappers.ClassWrapper;
+import eu.mikroskeem.test.shuriken.reflect.classes.TestClassFour;
 import eu.mikroskeem.test.shuriken.reflect.classes.TestClassOne;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -34,5 +35,24 @@ public class MethodReflectionTester {
         double expected = 'a';
         double actual = cw.invokeMethod("d", char.class);
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testClassArrayMethodInvoke() throws Exception {
+        ClassWrapper<TestClassFour> cw = Reflect.wrapClass(TestClassFour.class);
+        Double[] actual = cw.invokeMethod("get2", Double[].class);
+        Assertions.assertEquals(Double.valueOf(-1D), actual[0]);
+        Assertions.assertEquals(Double.valueOf(-1D), actual[1]);
+        Assertions.assertEquals(Double.valueOf(-1D), actual[2]);
+    }
+
+    @Test
+    public void testClassPrimitiveArrayMethodInvoke() throws Exception {
+        ClassWrapper<TestClassFour> cw = Reflect.wrapClass(TestClassFour.class);
+        double[] expected = new double[]{-1, -1, -1};
+        double[] actual = cw.invokeMethod("get", double[].class);
+        Assertions.assertEquals(-1D, actual[0]);
+        Assertions.assertEquals(-1D, actual[1]);
+        Assertions.assertEquals(-1D, actual[2]);
     }
 }
---
+++ reflect/src/test/java/eu/mikroskeem/test/shuriken/reflect/classes/TestClassFour.java
@@ -0,0 +1,13 @@
+package eu.mikroskeem.test.shuriken.reflect.classes;
+
+public class TestClassFour {
+    public static double[] a = new double[]{-2, -2, -2};
+
+    public static double[] get(){
+        return new double[]{-1, -1, -1};
+    }
+
+    public static Double[] get2(){
+        return new Double[]{-1D, -1D, -1D};
+    }
+}
