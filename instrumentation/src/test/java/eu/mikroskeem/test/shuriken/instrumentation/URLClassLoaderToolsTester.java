bda9749a6aef794b13f9aba7d170575ba58657e3
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-07-12 19:02:52 +0300

    Add URLClassLoaderTools tests

---
+++ instrumentation/src/test/java/eu/mikroskeem/test/shuriken/instrumentation/URLClassLoaderToolsTester.java
@@ -0,0 +1,52 @@
+package eu.mikroskeem.test.shuriken.instrumentation;
+
+import eu.mikroskeem.shuriken.common.ToURL;
+import eu.mikroskeem.shuriken.instrumentation.ClassLoaderTools;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+/**
+ * @author Mark Vainomaa
+ */
+public class URLClassLoaderToolsTester {
+    @Test
+    public void testUCLTools() throws Exception {
+        //URLClassLoader ucl = new URLClassLoader(new URL[0]);
+        URLClassLoader ucl = (URLClassLoader) ClassLoader.getSystemClassLoader();
+        ClassLoaderTools.URLClassLoaderTools uclTools = new ClassLoaderTools.URLClassLoaderTools(ucl);
+
+        Assertions.assertThrows(IllegalStateException.class, () ->
+                uclTools.addURL(new URL("https://mikroskeem.eu/stuff/fernflower.jar"))
+        );
+        uclTools.addURL(ToURL.to(downloadTestJar()));
+        uclTools.resetCache();
+    }
+
+
+    private Path downloadTestJar() throws Exception {
+        /* aopalliance library for test, hopefully this won't disappear */
+        String url = "https://repo.maven.apache.org/maven2/aopalliance/aopalliance/1.0/aopalliance-1.0.jar";
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        Path file = Files.createTempFile("testlib", ".jar");
+        try(FileChannel fc = new FileOutputStream(file.toFile()).getChannel()) {
+            fc.transferFrom(
+                    Channels.newChannel(connection.getInputStream()),
+                    0,
+                    Long.MAX_VALUE
+            );
+        }
+
+        return file;
+    }
+}
